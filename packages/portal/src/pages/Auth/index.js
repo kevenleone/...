7698cb5daa7f8df0b5427c2ddb2c42412796186f
Keven <keven.santos.sz@gmail.com>
2020-09-27 23:02:21 -0300

    chore: sigin page

---
+++ packages/portal/src/pages/Auth/index.js
@@ -0,0 +1,60 @@
+import './index.css'
+
+import { ClayInput } from '@clayui/form'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function SignIn () {
+  return (
+    <div id="wrapper">
+      <div id="left">
+        <div id="signin">
+          <div className="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Liferay-logo-full-color-2x.png" alt="Alural Xight" />
+          </div>
+          <form>
+            <div>
+              <label htmlFor="email">
+                Email or Username
+                <ClayInput
+                  id="email" name="email" type="text" className="text-input" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="password">
+                Password
+                <ClayInput id="password" name="password" type="password" className="text-input" />
+              </label>
+            </div>
+            <button type="button" className="primary-btn">
+              Sign In
+            </button>
+          </form>
+          <div className="links">
+            <Link to="/forgot">Forgot Password</Link>
+            <Link to="/signin1">Sign in with company or school</Link>
+          </div>
+          <div className="or">
+            <hr className="bar" />
+            <span>OR</span>
+            <hr className="bar" />
+          </div>
+          <Link to="/create" className="secondary-btn"> Create an account </Link>
+        </div>
+        <footer id="main-footer">
+          <p>Copyright &copy; 2020, All Rights Reserved</p>
+          <div>
+            <Link to="terms">
+              Terms of use / &ensp;
+            </Link>
+            <Link to="/policies">Privacy Policies</Link>
+          </div>
+        </footer>
+      </div>
+      <div id="right">
+        <div id="showcase">
+        </div>
+      </div>
+    </div>
+  )
+}
--- packages/portal/src/routes.js
+++ packages/portal/src/routes.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import ApplicationBar from './components/ApplicationBar'
+import Auth from './pages/Auth'
 import Home from './pages/Home'
 import Pipefy from './pages/Pipefy'
 
 const Routes = () => (
   <BrowserRouter>
-    <ApplicationBar />
     <Switch>
-      <Route path="/" exact component={Pipefy} />
-      <Route path="/home" exact component={Home} />
+      <Route path="/auth" exact component={Auth} />
+      <>
+        <ApplicationBar />
+        <Route path="/" exact component={Pipefy} />
+        <Route path="/home" exact component={Home} />
+      </>
     </Switch>
   </BrowserRouter>
 )
